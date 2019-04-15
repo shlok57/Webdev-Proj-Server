ef7d632a0586af6cb146124ad47692acecbe355e
Gaurav Gandhi <gaurav.g101@gmail.com>
2019-04-14 23:31:29 -0400

    Added validators in recipe schema

--- data/models/recipe/recipe.schema.server.js
+++ data/models/recipe/recipe.schema.server.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const recipeSchema = mongoose.Schema({
-    _id: Number,
-    Name: String,
-    Date_Created: Date,
-    Moderator_ID: {type: mongoose.Schema.Types.Number, ref: 'UserModel'},
-    Ingredients: String,
-    Steps: [String],
+    _id: {type: Number, required: true},
+    Name: {type: String, required: true},
+    Date_Created: {type: Date, required: true},
+    Moderator_ID: {type: mongoose.Schema.Types.Number, ref: 'UserModel', required: true},
+    Ingredients: {type: String, required: true},
+    Steps: [{type: String, required: true}],
     Image_URL: String,
     Video_URL: String,
     Cuisine: String,
