a91400bfeac619be176484ce0bb80353aabc8c65
Gaurav Gandhi <gaurav.g101@gmail.com>
2019-04-17 22:20:20 -0400

    Updated recipe service with new methods

--- services/recipe.service.server.js
+++ services/recipe.service.server.js
@@ -2,6 +2,9 @@ module.exports = app => {
 
     app.post('/api/recipe', createRecipe);
     app.get('/api/recipe', findAllRecipes);
+    app.get('/api/recipe/:id', findRecipeById);
+    app.put('/api/recipe/:id', updateRecipe);
+    app.delete('/api/recipe/:id', removeRecipe);
 }
 
 var recipeDao = require('../data/daos/recipe.dao.server');
@@ -20,4 +23,24 @@ findAllRecipes = (req, res) => {
 
     recipeDao.findAllRecipes()
         .then(recipes => res.send(recipes));
+}
+
+findRecipeById = (req, res) => {
+
+    var id =req.params['id'];
+    recipeDao.findById(id)
+        .then(recipe => res.send(recipe));
+}
+
+updateRecipe = (req, res) => {
+    var id = req.params['id'];
+    var updatedRecipe = req.body;
+    recipeDao.updateRecipe(id, updatedRecipe)
+        .then(recipe => res.send(recipe));
+}
+
+removeRecipe = (req, res) => {
+    var id = req.params['id'];
+    recipeDao.removeRecipe(id)
+        .then(response => res.send(response));
 }
